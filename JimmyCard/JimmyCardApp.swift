b99c4806b589742384203e3030dcce7425df15a6
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-07-19 09:52:51 -0400

    Initial Commit

---
+++ JimmyCard/JimmyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JimmyCardApp.swift
+//  JimmyCard
+//
+//  Created by Jimmy Ghelani on 2020-07-19.
+//
+
+import SwiftUI
+
+@main
+struct JimmyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
