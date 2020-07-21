9aa7acac8084734e43306d07099cb413889989c3
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-07-21 07:10:40 -0400

    Swift UI basics

--- JimmyCard/ContentView.swift
+++ JimmyCard/ContentView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.0)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Jimmy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Jimmy Ghelani")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("Front End Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "416-880-9896", imageName: "phone.fill")
+                InfoView(text: "ghelanijimmy@jgdesigns.ca", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
---
+++ JimmyCard/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  JimmyCard
+//
+//  Created by Jimmy Ghelani on 2020-07-21.
+//
+
+import SwiftUI
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50, alignment: .center)
+            .foregroundColor(.white)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.green)
+                    Text(text)
+                        .foregroundColor(.black)
+                }
+            )
+            .padding(.all)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
